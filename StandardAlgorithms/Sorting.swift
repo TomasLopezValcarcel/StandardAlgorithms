c7ca68dcfdb25f3897107cd3d28b0b3005d84563
Tomas Lopez-Valcarcel <lopez-valcarcel.t@etoncollege.org.uk>
2023-11-10 17:12:47 +0000

    adding automated tests

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,14 @@
+//
+//  Sorting.swift
+//  StandardAlgorithms
+//
+//  Created by Lopez-Valcarcel, Tomas (CDNP) on 10/11/2023.
+//
+
+import Foundation
+
+class Sorting{
+    func bubbleSort(_ numbers: [Int]) -> [Int] {
+        return [1,3,4,5,7]
+    }
+}
