371970b3247047d46cfea4f26bcba17a303fcc97
Tomas Lopez-Valcarcel <lopez-valcarcel.t@etoncollege.org.uk>
2023-11-19 22:51:41 +0000

    linear search (sort of)

--- StandardAlgorithms/Searching.swift
+++ StandardAlgorithms/Searching.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+
+//import Foundation
+//
+//class Searching{
+//
+//    func linearSearch(_ numbers: [Int], searchTerm: Int) -> Int {
+//        for i in 0...numbers.count{
+//            if numbers[i] == searchTerm{
+//                return i
+//            }
+//        }
+//
+//    }
+//
+//}
+
+
+//import XCTest
+//
+//final class SearchingTests: XCTestCase {
+//
+//    struct SearchingTestCase {
+//        let data: [Int]
+//        let expected: [Int]
+//    }
+//
+//
+//    func testLinearSearchWithArraysOfIntegersAndASearchTermReturnsPositionOfFoundTermInArray() {
+//        //arrange
+//        let searching = Searching()
+//
+//        let testcases = [(data: [4,7,3,6,3], searchTerm: 6, expected: 3)]
+//
+//        //act
+//        //assert
+//        for testCase in testcases {
+//            let actual = searching.linearSearch(testCase.data, searchTerm: testCase.searchTerm)
+//            XCTAssertEqual(actual, testCase.expected)
+//        }
+//    }
+//}
